11791e5eba4b54ff8ea3cd9fa55117b3c2a5c0ec
Gaurang Pandey <123499291+Gaurang-Pandey@users.noreply.github.com>
2025-03-15 16:12:18 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,18 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
 import { SplashScreen } from "./screens/SplashScreen";
 import { createStackNavigator } from "@react-navigation/stack";
-import { HomeScreen } from "./screens/HomeScreen";
 import { NavigationContainer } from "@react-navigation/native";
-import {createDrawerNavigator} from "@react-navigation/drawer";
-import {CategoryScreen} from "./screens/CategoryScreen";
 import { DrawerNavigation } from "./navigation/DrawerNavigation";
 
 export default function App() {
-
   const Stack = createStackNavigator();
-  const Drawer = createDrawerNavigator();
 
-  // Stack for Splash Screen and Home Screen
   const MainStack = () => (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="Splash" component={SplashScreen} />
@@ -20,13 +13,9 @@ export default function App() {
     </Stack.Navigator>
   );
 
-
-
   return (
-
     <NavigationContainer>
       <MainStack />
     </NavigationContainer>
   );
 }
-
